8f42408670a1cd70470f62f4d4bbc75d0b2d25fe
Jeff Tsai <tsai81926@gmail.com>
2020-10-25 17:26:12 +0800

    add count

--- num.py
+++ num.py
@@ -4,14 +4,18 @@
 #猜錯要告訴使用者比數自大/小
 
 import random
+count = 0
 r = random.randint(1 ,100)
 while True:
+	count = count + 1 #快寫法: "count += 1"
 	num = input('請猜數字: ')
 	num = int(num)
 	if num == r:
 		print('恭喜猜中了!!')
+		print('這是你猜的第', count,'次')
 		break
 	elif num > r:
 		print('比答案大')
 	elif num < r:
 		print('比答案小')
+	print('這是你猜的第', count,'次')
